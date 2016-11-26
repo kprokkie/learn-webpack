586646fdbae30d7b9b0c984afcc48e98cf7cabd9
kprokkie <kumarpradeep1010@gmail.com>
2016-11-26 17:57:00 -0600

    react component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-const $ = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-$('#target').html('Hello Webpack !!');
+// To pull in all CSS files
+require('./style.css');
+
+class Message extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <p>{this.props.message}</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Message title='Pradeep Kumar' message='Can you tell me about Webpack ?'/>,
+    document.getElementById('react-container'));
 
