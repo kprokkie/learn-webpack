a0bc276bc5deedb678d7db82501f96a6fa3f59f0
kprokkie <kumarpradeep1010@gmail.com>
2016-11-26 15:11:55 -0600

    webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
         path: 'build',
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015','react']
+                }
+            }
+        ]
     }
 };
